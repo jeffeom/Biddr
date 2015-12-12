baa8cd266dd4dc44d2f3f98bffd77c980a1ae4b7
Jeff <jeffeom@gmail.com>
2015-12-11 17:19:44 -0800

    Added ajaxify on bid only

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,10 +3,20 @@ class BidsController < ApplicationController
     @auction = Auction.find(params[:auction_id])
     @bid   = @auction.bids.new bid_params
     @bid.user = current_user
-    if @bid.save
-      redirect_to auction_path(@auction), notice: "Please Bid"
-    else
-      render "/auctions/show"
+    @bid.auction = @auction
+    # if @bid.save
+    #   redirect_to auction_path(@auction), notice: "Please Bid"
+    # else
+    #   render "/auctions/show"
+    # end
+    respond_to do |format|
+      if @bid.save
+        format.html { redirect_to auction_path(@auction), notice: "Bid has been submitted successfully!" }
+        format.js {render :create_success}
+      else
+        format.html { render "auctions/show "}
+        format.js { render :create_failure }
+      end
     end
   end
 
